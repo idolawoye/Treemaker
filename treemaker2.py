a8540f80dc5c9b8269bdd689ed97c19669aa55da
Idowu Olawoye <idowuolawoye@gmail.com>
2018-07-09 16:52:30 +0100

    Update treemaker2.py

--- treemaker2.py
+++ treemaker2.py
@@ -4,13 +4,14 @@ A python3 script that takes alignment files and creates a treefile using SNP var
 print("TREEMAKER v0.1 is a phylogeny tree builder that takes alignment files and constructs a relationship tree. Written by Idowu Olawoye @idowuolawoye\n")
 
 import argparse
+from Bio import Seq
+from Bio import AlignIO
+from Bio.Phylo.PAML import baseml
 
 parser = argparse.ArgumentParser(description='A phylogeny tree builder.')
 parser.add_argument('-V','--version',action='version', version='TREEMAKER v0.1 Released 27/06/2018')
 
 args= parser.parse_args()
-from Bio import Seq
-from Bio import AlignIO
 
 aln = str(input("Enter path to Alignment file:"))
  
@@ -19,6 +20,7 @@ while aln:
 		1.Parsimony Tree constructor
 		2.Distance Tree constructor (Neighbour Joining)
 		3.Distance Tree constructor (UPGMA)
+		4.Maximum Likelihood using TN93 model
 		""")
 	align=input("What model will you like to use?")
 	if align=="1":
@@ -36,7 +38,7 @@ while aln:
 		print(pars_tree)
 		print('\n')
 		from Bio import Phylo
-#Generates parsimony tree using maximum likelihood. Tree file is saved in newick format
+
 		Phylo.write(pars_tree, aln+".newick", 'newick')
 		print("DONE.\n")
 		print('Tree file can be found in', aln+'.newick')
@@ -56,7 +58,7 @@ while aln:
 		print(pars_tree)
 		print('\n')
 		from Bio import Phylo
-#Generates parsimony tree using maximum likelihood. Tree file is saved in newick format
+
 		Phylo.write(pars_tree, aln+".newick", 'newick')
 		print("DONE.\n")
 		print('Tree file can be found in', aln+'.newick')
@@ -76,12 +78,55 @@ while aln:
 		print(pars_tree)
 		print('\n')
 		from Bio import Phylo
-#Generates parsimony tree using maximum likelihood. Tree file is saved in newick format
+
 		Phylo.write(pars_tree, aln+".newick", 'newick')
 		print("DONE.\n")
 		print('Tree file can be found in', aln+'.newick')
 		Phylo.draw(pars_tree)
 		break
+	if align =="4":
+		out = str(input("Enter path to output file and filename:"))
+		work_dir = str(input("Enter path to working directory:"))
+		align = AlignIO.parse (aln, 'phylip')
+		for alignment in align:
+			print(alignment)
+			print('')
+			print('Building Tree...\n')
+		from Bio.Phylo.TreeConstruction import *
+		scorer= ParsimonyScorer()
+		searcher = NNITreeSearcher(scorer)
+		constructor = ParsimonyTreeConstructor(searcher)
+		pars_tree = constructor.build_tree(alignment)
+		print(pars_tree)
+		print('\n')
+		from Bio import Phylo
+		Phylo.write(pars_tree, aln+".newick", 'newick')
+		with open(aln,'r') as original:
+			allLines = original.readlines()
+		allLines[0] = allLines[0].rstrip() + "I\n"
+		modifyFile = open(aln,'w')
+		for i in allLines:
+			modifyFile.write(i)
+		modifyFile.close()
+
+		bml = baseml.Baseml()
+		bml.alignment = aln
+		bml.tree = aln+".newick"
+		bml.out_file = out
+		bml.working_dir = work_dir
+		bml.set_options(runmode=2, model=6)
+		bml.run()
+		
+		with open (out, 'r') as result:
+			lines = result.readlines()
+		print("Best tree found:")
+		print(lines[-1])
+		last = lines[-1]
+		newtree = open(out+".newick", "w")
+		newtree.write(last)
+		newtree.close()
+		print("Maximum Likelihood tree built with TN93 model found in:")
+		print(work_dir)
 	else:
 		print("\n Not valid")
 		break
